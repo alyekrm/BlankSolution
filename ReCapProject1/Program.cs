bfcc148224c9bea04997aa2c263dc0f172609375
aly ekrm <alyekrm@hotmail.com>
2021-02-17 19:36:16 +0300

    9hw

--- ReCapProject1/Program.cs
+++ ReCapProject1/Program.cs
@@ -14,15 +14,36 @@ namespace ReCapProject1
         {
             BusinessManager businessManager = new BusinessManager(new EfCarDal());
 
+            businessManager.Update(new Car { Id = 5, Name = "cba", BrandId = 1, ColorId = 1, DailyPrice = 00 });
 
             Console.WriteLine("GetALL");
 
 
-            foreach (var item in businessManager.GetCarDetails())
-            {
-                Console.WriteLine("{0},{1},{2},{3}", item.CarName, item.BrandName, item.ColorName, item.DailyPrice);
-            }
+            Print(businessManager);
+            
+            Car car = NewCar(businessManager);
 
+            businessManager.Add(car);
+            
+            Console.WriteLine("------Added-------");
+
+            Print(businessManager);
+            
+            businessManager.Delete(car);
+
+            Console.WriteLine("-----Deleted-----");
+
+            Print(businessManager);
+
+            Console.WriteLine("-----Updated-----");
+
+            businessManager.Update(new Car { Id = 5, Name = "xxx", BrandId = 1, ColorId = 1, DailyPrice = 00 });
+
+            Print(businessManager);
+        }
+
+        private static Car NewCar(BusinessManager businessManager)
+        {
             Car car = new Car() { Name = "a" };
             businessManager.Add(car);
             car.Name = "abc";
@@ -38,28 +59,12 @@ namespace ReCapProject1
                 var id = item.Id;
                 car.Id = id + 1;
             }
-  
-            businessManager.Add(car);
-            Console.WriteLine("------Added-------");
 
-            foreach (var item in businessManager.GetCarDetails())
-            {
-                Console.WriteLine("{0},{1},{2},{3}", item.CarName, item.BrandName, item.ColorName, item.DailyPrice);
-            }
-            businessManager.Delete(car);
+            return car;
+        }
 
-            Console.WriteLine("-----Deleted-----");
-
-            foreach (var item in businessManager.GetCarDetails())
-            {
-                Console.WriteLine("{0},{1},{2},{3}", item.CarName, item.BrandName, item.ColorName, item.DailyPrice);
-            }
-            
-      
-            Console.WriteLine("-----Updated-----");
-
-            businessManager.Update(new Car { Id = 5, Name = "xxx", BrandId = 1,ColorId=1,DailyPrice=00 });
-
+        private static void Print(BusinessManager businessManager)
+        {
             foreach (var item in businessManager.GetCarDetails())
             {
                 Console.WriteLine("{0},{1},{2},{3}", item.CarName, item.BrandName, item.ColorName, item.DailyPrice);
